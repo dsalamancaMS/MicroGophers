758539542cbe647987d0c0296dd7390e447e3a66
Denis Salamanca <desalama@microsoft.com>
2018-12-06 20:34:56 -0600

    added error handling

--- src/boss_gopher/boss_gohper.go
+++ src/boss_gopher/boss_gohper.go
@@ -7,11 +7,17 @@ import (
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		resp, _ := http.Get("http://localhost:8001/")
-		defer resp.Body.Close()
-		body, _ := ioutil.ReadAll(resp.Body)
-		workerResponse := string(body)
-		w.Write([]byte("Boss Gopher => " + workerResponse))
+		resp, err := http.Get("http://localhost:8001/")
+		if err != nil {
+			workerResponse := "Worker is lazy"
+			w.Write([]byte("Boss Gopher => " + workerResponse))
+		} else {
+			body, _ := ioutil.ReadAll(resp.Body)
+			workerResponse := string(body)
+			w.Write([]byte("Boss Gopher => " + workerResponse))
+			defer resp.Body.Close()
+		}
+
 	})
 	http.ListenAndServe(":8000", nil)
 }
