51b683de743d50d72e4ae761c6d71ef05f3c3572
Denis Salamanca <desalama@microsoft.com>
2018-12-07 10:45:23 -0600

    Gophers Ready

--- src/worker_gopher/worker_gopher.go
+++ src/worker_gopher/worker_gopher.go
@@ -1,12 +1,23 @@
 package main
 
 import (
+	"io/ioutil"
 	"net/http"
 )
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Worker Gopher => "))
+		resp, err := http.Get("http://localhost:8002/")
+		if err != nil {
+			workerResponse := "Minon Gopher slain"
+			w.Write([]byte("Worker Gopher => " + workerResponse))
+		} else {
+			body, _ := ioutil.ReadAll(resp.Body)
+			workerResponse := string(body)
+			w.Write([]byte("Worker Gopher => " + workerResponse))
+			defer resp.Body.Close()
+		}
 	})
+
 	http.ListenAndServe(":8001", nil)
 }
